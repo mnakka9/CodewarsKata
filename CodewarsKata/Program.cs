5f17575dc802b07ebf85e06d301775228e4837c7
Mahesh Nakka <Mahesh_Nakka@epam.com>
2017-11-16 16:44:24 +0300

    Add project files.

---
+++ CodewarsKata/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodewarsKata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
